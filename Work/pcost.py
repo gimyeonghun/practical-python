a7abec862e0f9bc6321dbc9ab7b44d5d830f8a35
gimyeonghun <67149286+gimyeonghun@users.noreply.github.com>
2020-06-20 01:12:59 +1000

    Exercise 1.29

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,13 +2,21 @@
 #
 # Exercise 1.27
 
-tcost = 0.0
 
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f).split(',')
-    for line in f:
-       row = line.split(',')
-       tcost = tcost + (float(row[1]) * float(row[2]))
 
-print(f"Total cost {tcost}")
+def portfolio_cost(filename):
+    tcost = 0.0
+    with open(filename, 'rt') as f:
+        headers = next(f).split(',')
+        for line in f:
+            row = line.split(',')
+            print(tcost)
+            tcost = tcost + (float(row[1]) * float(row[2]))
+    return tcost
+
+
+
+cost = portfolio_cost('Data/portfolio.csv')
+
+print(f"Total cost {cost}")
        
